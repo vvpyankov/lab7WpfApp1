dc9219cfb31f2c5e5475ece7e7335ced4d142fb4
vvpyankov@mail.ru <vvpyankov@mail.ru>
2022-03-09 21:17:19 +0300

    Добавьте файлы проекта.

---
+++ lab7WpfApp1/MyCommands.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Input;
+
+namespace lab7WpfApp1
+{
+    internal class MyCommands
+    {
+        public static RoutedUICommand Exit { get; set; }
+
+        static MyCommands()
+        {
+            InputGestureCollection inputs = new InputGestureCollection();
+            inputs.Add(new KeyGesture(Key.T, ModifierKeys.Control, "Ctrl+T"));
+            //Exit = new RoutedCommand("Exit", typeof(MyCommands), inputs );
+            Exit = new RoutedUICommand("Exit", "Exit", typeof(MyCommands), inputs );
+        }
+    }
+}
